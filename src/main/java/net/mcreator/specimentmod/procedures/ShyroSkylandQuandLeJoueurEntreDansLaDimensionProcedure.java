de042cb521f17ba64bd7e04e21e881bfe74465f9
specimenmob <unconfigured@null.spigotmc.org>
2024-07-25 17:30:16 +0200

    Maj ShyroSkyland 1

---
+++ src/main/java/net/mcreator/specimentmod/procedures/ShyroSkylandQuandLeJoueurEntreDansLaDimensionProcedure.java
@@ -0,0 +1,20 @@
+package net.mcreator.specimentmod.procedures;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.CommandSource;
+
+public class ShyroSkylandQuandLeJoueurEntreDansLaDimensionProcedure {
+	public static void execute(Entity entity) {
+		if (entity == null)
+			return;
+		{
+			Entity _ent = entity;
+			if (!_ent.level().isClientSide() && _ent.getServer() != null) {
+				_ent.getServer().getCommands().performPrefixedCommand(new CommandSourceStack(CommandSource.NULL, _ent.position(), _ent.getRotationVector(), _ent.level() instanceof ServerLevel ? (ServerLevel) _ent.level() : null, 4,
+						_ent.getName().getString(), _ent.getDisplayName(), _ent.level().getServer(), _ent), "execute as @s in speciment_mod:shyro_skyland run tp @s 100 100 0");
+			}
+		}
+	}
+}
--- src/main/java/net/mcreator/specimentmod/world/dimension/ShyroSkylandDimension.java
+++ src/main/java/net/mcreator/specimentmod/world/dimension/ShyroSkylandDimension.java
@@ -1,24 +1,32 @@
 
 package net.mcreator.specimentmod.world.dimension;
 
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.client.event.RegisterDimensionSpecialEffectsEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.api.distmarker.Dist;
 
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
 
+import net.mcreator.specimentmod.procedures.ShyroSkylandQuandLeJoueurEntreDansLaDimensionProcedure;
+
+@EventBusSubscriber
 public class ShyroSkylandDimension {
 	@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 	public static class ShyroSkylandSpecialEffectsHandler {
 		@SubscribeEvent
 		public static void registerDimensionSpecialEffects(RegisterDimensionSpecialEffectsEvent event) {
-			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(DimensionSpecialEffects.OverworldEffects.CLOUD_LEVEL, true, DimensionSpecialEffects.SkyType.NORMAL, false, false) {
+			DimensionSpecialEffects customEffect = new DimensionSpecialEffects(Float.NaN, true, DimensionSpecialEffects.SkyType.NONE, false, false) {
 				@Override
 				public Vec3 getBrightnessDependentFogColor(Vec3 color, float sunHeight) {
-					return color.multiply(sunHeight * 0.94 + 0.06, sunHeight * 0.94 + 0.06, sunHeight * 0.91 + 0.09);
+					return color;
 				}
 
 				@Override
@@ -27,6 +35,18 @@ public class ShyroSkylandDimension {
 				}
 			};
 			event.register(new ResourceLocation("speciment_mod:shyro_skyland"), customEffect);
+		}
+	}
+
+	@SubscribeEvent
+	public static void onPlayerChangedDimensionEvent(PlayerEvent.PlayerChangedDimensionEvent event) {
+		Entity entity = event.getEntity();
+		Level world = entity.level();
+		double x = entity.getX();
+		double y = entity.getY();
+		double z = entity.getZ();
+		if (event.getTo() == ResourceKey.create(Registries.DIMENSION, new ResourceLocation("speciment_mod:shyro_skyland"))) {
+			ShyroSkylandQuandLeJoueurEntreDansLaDimensionProcedure.execute(entity);
 		}
 	}
 }
